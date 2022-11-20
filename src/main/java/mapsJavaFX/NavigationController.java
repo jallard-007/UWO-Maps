14689f8f7c1a643206de445adb75f7b8211a08a8
Rose Gao <rgao65@uwo.ca>
2022-11-20 14:33:43 -0500

    Fixing "go back" functionality

--- src/main/java/mapsJavaFX/NavigationController.java
+++ src/main/java/mapsJavaFX/NavigationController.java
@@ -1,15 +1,21 @@
 package mapsJavaFX;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import maps.Application;
+
 import java.io.IOException;
 
 public class NavigationController {
+
+  @FXML  private AnchorPane menuBar;
 
   public void goToProfile(ActionEvent event) throws IOException {
     FXMLLoader fxmlLoader = new FXMLLoader(SignupController.class.getResource("/profile.fxml"));
@@ -26,8 +32,25 @@ public class NavigationController {
   public void goBack(ActionEvent event) throws IOException {
     FXMLLoader fxmlLoader = new FXMLLoader(SignupController.class.getResource("/mainView.fxml"));
     Scene scene = new Scene(fxmlLoader.load());
-    changeScene(scene, event);
+    Stage stage = (Stage) menuBar.getScene().getWindow();
+    stage.setX(23);
+    stage.setY(20);
+    stage.setScene(scene);
+    stage.show();
+    MainController controller = fxmlLoader.getController();
+    Application app = new Application();
+    try {
+      app.loadData();
+    } catch (IOException e) {
+      System.out.println(e.getMessage());
+      e.printStackTrace();
+      System.exit(12);
+    }
+    ControllerMediator.getInstance().registerMapViewController(controller.getMapViewController());
+    controller.getSearchPOIController().addPOIs(app.getPoiLocations());
+    controller.getMapViewController().addBuildings(app.getBuildings());
   }
+
 
   public void logout(ActionEvent event) throws IOException {
     Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
@@ -41,13 +64,6 @@ public class NavigationController {
       changeScene(scene, event);
     }
   }
-
-  //  public void goToLogin(ActionEvent event) throws IOException {
-//    FXMLLoader fxmlLoader = new FXMLLoader(SignupController.class.getResource("/login.fxml"));
-//    Scene scene = new Scene(fxmlLoader.load());
-//    changeScene(scene, event);
-//  }
-
   private void changeScene(Scene scene, ActionEvent event) {
     Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
     stage.setScene(scene);
