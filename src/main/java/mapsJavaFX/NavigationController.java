3c6948c4b70f37d99ae3d5a3770e4760f1ec323b
Rose Gao <rgao65@uwo.ca>
2022-11-20 13:38:38 -0500

    Setting log out and exit confirmation options

--- src/main/java/mapsJavaFX/NavigationController.java
+++ src/main/java/mapsJavaFX/NavigationController.java
@@ -4,15 +4,12 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 import java.io.IOException;
 
 public class NavigationController {
-  public void goToLogin(ActionEvent event) throws IOException {
-    FXMLLoader fxmlLoader = new FXMLLoader(SignupController.class.getResource("/login.fxml"));
-    Scene scene = new Scene(fxmlLoader.load());
-    changeScene(scene, event);
-  }
 
   public void goToProfile(ActionEvent event) throws IOException {
     FXMLLoader fxmlLoader = new FXMLLoader(SignupController.class.getResource("/profile.fxml"));
@@ -31,6 +28,25 @@ public class NavigationController {
     Scene scene = new Scene(fxmlLoader.load());
     changeScene(scene, event);
   }
+
+  public void logout(ActionEvent event) throws IOException {
+    Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+    alert.setTitle("Logout");
+    alert.setHeaderText("You're about to log out");
+    alert.setContentText("Are you sure?");
+
+    if (alert.showAndWait().get() == ButtonType.OK){
+      FXMLLoader fxmlLoader = new FXMLLoader(SignupController.class.getResource("/login.fxml"));
+      Scene scene = new Scene(fxmlLoader.load());
+      changeScene(scene, event);
+    }
+  }
+
+  //  public void goToLogin(ActionEvent event) throws IOException {
+//    FXMLLoader fxmlLoader = new FXMLLoader(SignupController.class.getResource("/login.fxml"));
+//    Scene scene = new Scene(fxmlLoader.load());
+//    changeScene(scene, event);
+//  }
 
   private void changeScene(Scene scene, ActionEvent event) {
     Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
--- src/main/java/mapsJavaFX/WesternMaps.java
+++ src/main/java/mapsJavaFX/WesternMaps.java
@@ -1,6 +1,8 @@
 package mapsJavaFX;
 
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import java.io.IOException;
@@ -19,5 +21,26 @@ public class WesternMaps extends javafx.application.Application {
     stage.setResizable(false);
     stage.setScene(scene);
     stage.show();
+
+    stage.setOnCloseRequest(event -> {
+      event.consume();
+      try {
+        exit(stage);
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
+    });
+  }
+
+  public void exit(Stage stage) throws IOException {
+    Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+    alert.setTitle("Logout");
+    alert.setHeaderText("You're about to exit from the program");
+    alert.setContentText("Are you sure?");
+
+    if (alert.showAndWait().get() == ButtonType.OK){
+      System.out.println("The program was exited successfully.");
+      stage.close();
+    }
   }
 }
