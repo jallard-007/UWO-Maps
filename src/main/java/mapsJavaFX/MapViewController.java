1c1497694867dc250e01edfdf7ce25ab1612e328
Justin Nicolas Allard <jallard9@uwo.ca>
2022-11-27 03:36:57 -0500

    Make zoom feature work with poi buttons

--- src/main/java/mapsJavaFX/MapViewController.java
+++ src/main/java/mapsJavaFX/MapViewController.java
@@ -22,8 +22,6 @@ public class MapViewController {
   @FXML
   private Slider zoomBar;
   @FXML
-  private StackPane stackPane;
-  @FXML
   private TabPane tabPane;
   Application app;
   private List<POIButton>[] poiButtons;
@@ -31,6 +29,7 @@ public class MapViewController {
   @SuppressWarnings("unchecked")
   public void setApp(Application app) {
     this.app = app;
+    POIButton.setSlider(zoomBar);
     this.poiButtons = (ArrayList<POIButton>[]) new ArrayList[POIType.values().length];
     for (int i = 0; i < POIType.values().length; ++i) {
       this.poiButtons[i] = new ArrayList<>();
@@ -61,18 +60,15 @@ public class MapViewController {
 
         Pane poiPane = new Pane();
         stackPane.getChildren().add(imageView);
-        // stackPane.getChildren().add(poiPane);
+        stackPane.getChildren().add(poiPane);
         scrollPane.setContent(stackPane);
         floorTab.setContent(scrollPane);
         floorTab.setClosable(false);
         buildingTabPane.getTabs().add(floorTab);
 
         zoomBar.valueProperty().addListener((o, oldV, newV) -> {
-          var x = scrollPane.getHvalue();
-          var y = scrollPane.getVvalue();
-          imageView.setFitWidth(image.getWidth() * newV.doubleValue());
-          scrollPane.setHvalue(x);
-          scrollPane.setVvalue(y);
+          stackPane.setScaleX(newV.doubleValue());
+          stackPane.setScaleY(newV.doubleValue());
         });
       }
     }
@@ -80,23 +76,23 @@ public class MapViewController {
     String buildingName = "";
     String floorName = "";
     Pane currPane = null;
-    // for (POILocation poiLocation : this.app.getPoiLocations()) {
-    // POIButton poiButton = new POIButton(poiLocation);
-    // poiButtons[poiLocation.getPOI().getPOIType().ordinal()].add(poiButton);
-    // poiButton.makeDraggable();
+    for (POILocation poiLocation : this.app.getPoiLocations()) {
+      POIButton poiButton = new POIButton(poiLocation);
+      poiButtons[poiLocation.getPOI().getPOIType().ordinal()].add(poiButton);
+      poiButton.makeDraggable();
 
-    // poiButton.setLayoutX(poiLocation.getPOI().getPosition().getX());
-    // poiButton.setLayoutY(poiLocation.getPOI().getPosition().getY());
-    // if (!buildingName.equals(poiLocation.getBuilding().getName())
-    // || !floorName.equals(poiLocation.getFloor().getName())) {
-    // currPane = getPane(poiLocation);
-    // }
-    // if (currPane == null) {
-    // System.out.print("Error: Could not find the specified Pane | in MapViewController.setApp");
-    // System.exit(55);
-    // }
-    // currPane.getChildren().add(poiButton);
-    // }
+      poiButton.setLayoutX(poiLocation.getPOI().getPosition().getX());
+      poiButton.setLayoutY(poiLocation.getPOI().getPosition().getY());
+      if (!buildingName.equals(poiLocation.getBuilding().getName())
+          || !floorName.equals(poiLocation.getFloor().getName())) {
+        currPane = getPane(poiLocation);
+      }
+      if (currPane == null) {
+        System.out.print("Error: Could not find the specified Pane | in MapViewController.setApp");
+        System.exit(55);
+      }
+      currPane.getChildren().add(poiButton);
+    }
   }
 
   public void goToPOI(POILocation poiLocation) {
@@ -113,8 +109,8 @@ public class MapViewController {
     double errorX = xRatio - 0.5;
     double errorY = yRatio - 0.5;
 
-    scrollPane.setHvalue(xRatio + (errorX * 0.2));
-    scrollPane.setVvalue(yRatio + (errorY * 0.2));
+    scrollPane.setHvalue(0.5 + (zoomBar.getValue() * ((xRatio + (errorX * 0.20)) - 0.5)));
+    scrollPane.setVvalue(0.5 + (zoomBar.getValue() * ((yRatio + (errorY * 0.20)) - 0.5)));
 
   }
 
@@ -138,7 +134,7 @@ public class MapViewController {
         for (Tab floorTab : buildingTabPane.getTabs()) {
           if (floorTab.getText().equals(poiLocation.getFloor().getName())) {
             StackPane stak = (StackPane) ((ScrollPane) floorTab.getContent()).getContent();
-            return (Pane) stak.getChildren().get(1);
+            return (Pane) (stak.getChildren()).get(1);
           }
         }
       }
