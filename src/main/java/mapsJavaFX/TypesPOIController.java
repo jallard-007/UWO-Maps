27fc3322de133ffceafcbda486868beae8a846a5
Justin Nicolas Allard <jallard9@uwo.ca>
2022-11-29 21:51:14 -0500

    Update TypesPOIController.java

--- src/main/java/mapsJavaFX/TypesPOIController.java
+++ src/main/java/mapsJavaFX/TypesPOIController.java
@@ -2,22 +2,38 @@ package mapsJavaFX;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.CheckBoxListCell;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
+import javafx.util.Callback;
 import maps.POIType;
 
 public class TypesPOIController {
     @FXML
     private ListView<POIType> poiTypeList;
 
-    public TypesPOIController() {}
+    public TypesPOIController() {
+    }
 
     @FXML
     public void initialize() {
         poiTypeList.setPlaceholder(new Label("No Matching POIs"));
         poiTypeList.getItems().addAll(maps.POIType.values());
+        poiTypeList.setCellFactory(CheckBoxListCell.forListView(new Callback<POIType, ObservableValue<Boolean>>() {
+            @Override
+            public ObservableValue<Boolean> call(POIType item) {
+                BooleanProperty observable = new SimpleBooleanProperty();
+                observable.addListener((obs, wasSelected, isNowSelected) -> System.out
+                        .println("Check box for " + item + " changed from " + wasSelected + " to " + isNowSelected));
+                return observable;
+            }
+        }));
     }
 
     public void onPOIListMouseClick(MouseEvent mouseEvent) {
