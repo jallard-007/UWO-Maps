6bb8bd01ff21b52dfeed24f75cfa0d71261861c2
Gabby Niamat <gniamat@uwo.ca>
2022-12-01 16:35:37 -0500

    Building Unit tests finished

--- src/test/java/mapsTest/BuildingTest.java
+++ src/test/java/mapsTest/BuildingTest.java
@@ -2,28 +2,62 @@ package mapsTest;
 
 import maps.Building;
 import maps.Floor;
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+
 class BuildingTest {
+  Building building;
+
+  @BeforeEach
+  void setUp() {
+    building = new Building("TestBuilding");
+  }
+
+  @Test
+  @DisplayName("Verify Building's getName()")
+  void testBuildingName() {
+    assertEquals("TestBuilding", building.getName());
+  }
+
+  @Test
+  @DisplayName("Verify Building's getFloors()")
+  void testBuildingFloor() {
+    ArrayList<Floor> list = new ArrayList<>();
+    assertEquals(list, building.getFloors());
+  }
+
+  @Test
+  @DisplayName("Verify Building's toString()")
+  void testBuildingToString() {
+    String expected = "\nBuildingName: TestBuilding";
+    assertEquals(expected, building.toString());
+  }
+
+  @Test
+  @DisplayName("Verify Building's toJson()")
+  void testBuildingToJSON() {
+    assertEquals("\nBuildingName: TestBuilding", building.toString());
+  }
 
   @Test
   void getMatchingFloor() {
-    System.out.println("Building.getMatchingFloor");
-    Building instance = new Building("TestBuilding");
     Floor expected = new Floor(1, "some floor");
-    instance.getFloors().add(expected);
-    Floor result = instance.getMatchingFloor("some floor");
+    building.getFloors().add(expected);
+    Floor result = building.getMatchingFloor("some floor");
     assertEquals(result, expected);
   }
 
   @Test
   void getMatchingFloorNull() {
-    System.out.println("Building.getMatchingFloor | null");
-    Building instance = new Building("TestBuilding");
-    instance.getFloors().add(new Floor(1, "a floor"));
-    Floor result = instance.getMatchingFloor("some floor");
+    building.getFloors().add(new Floor(1, "a floor"));
+    Floor result = building.getMatchingFloor("some floor");
     assertNull(result);
   }
 }
