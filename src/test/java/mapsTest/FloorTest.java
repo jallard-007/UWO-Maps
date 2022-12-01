e7ff8804f2e1ca3dbd15713dad4857cab528d51d
Gabby Niamat <gniamat@uwo.ca>
2022-12-01 17:28:50 -0500

    added Pair unit tests

--- src/test/java/mapsTest/FloorTest.java
+++ src/test/java/mapsTest/FloorTest.java
@@ -7,9 +7,6 @@ import maps.Pair;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
-
-import java.util.ArrayList;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 
